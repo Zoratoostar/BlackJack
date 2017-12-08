b5069ce14f70e99470f47083d5a576fa5419f1f8
Zoratoostar <toevgeny@gmail.com>
2017-12-08 14:30:28 +0800

    PlayingCards optimization

--- playing_cards.rb
+++ playing_cards.rb
@@ -1,21 +1,36 @@
 
 module PlayingCards
-  RANKS = %i[2 3 4 5 6 7 8 9 10 J Q K A]
-  SUITS = %i[c d h s]
+  RANKS = %i[2 3 4 5 6 7 8 9 10 J Q K A].freeze
+  SUITS = %i[c d h s].freeze
   RANK_VALUES = {
     '2': 2, '3': 3, '4': 4, '5': 5,
     '6': 6, '7': 7, '8': 8, '9': 9,
     '10': 10, J: 10, Q: 10, K: 10, A: 1
-  }
+  }.freeze
+
+  class << self
+    def card_value(card)
+      RANK_VALUES.fetch(card.first)
+    end
+
+    private
+
+    def make_cards
+      cards = []
+      RANKS.each do |rank|
+        SUITS.each do |suit|
+          cards << [rank, suit]
+        end
+      end
+      cards
+    end
+  end
+
+  CARDS = make_cards.freeze
 
   class Deck
     def initialize
-      @cards = []
-      RANKS.each do |rank|
-        SUITS.each do |suit|
-          @cards << [rank, suit]
-        end
-      end
+      @cards = CARDS.dup
       @cards.shuffle!
     end
 
@@ -26,9 +41,5 @@ module PlayingCards
     def size
       @cards.size
     end
-  end
-
-  def card_value(card)
-    RANK_VALUES.fetch(card.first)
   end
 end
