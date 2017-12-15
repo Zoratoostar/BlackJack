d449c7c3d328ea4971c4e0f8f9bbe5fb16f439ce
Zoratoostar <toevgeny@gmail.com>
2017-12-15 20:12:24 +0800

    Interface

--- main.rb
+++ main.rb
@@ -1,8 +1,9 @@
 
 require_relative 'validation.rb'
 require_relative 'playing_cards'
+require_relative 'interface'
 require_relative 'player'
 require_relative 'game'
 
-game = Game.new
-game.start
+bj = Interface.new
+bj.start
